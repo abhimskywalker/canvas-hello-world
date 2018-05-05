87b90f6adcb5796542a529c4cf763111d0337785
Abhishek Malik <abhimskywalker@gmail.com>
2018-05-05 23:16:51 +0530

    Add a circle instead of a simple line
    Let’s make it move… next.

--- src/app.js
+++ src/app.js
@@ -9,14 +9,25 @@ const c = canvas.getContext('2d')
 canvas.width = window.innerWidth
 canvas.height = window.innerHeight
 
-// begin drawing a path
+// // begin drawing a path
+// c.beginPath();
+// // move to a starting point without drawing anything
+// c.moveTo(100,100);
+// // define a path line from starting point (x:100, y:100) to another point(x:300, y:100)
+// c.lineTo(300,100);
+// // actually draw the stroke of the path
+// c.stroke();
+// // close the path
+// c.closePath();
+
+// Time to draw a circle this time:
 c.beginPath();
-// move to a starting point without drawing anything
-c.moveTo(100,100);
-// define a path line from starting point (x:100, y:100) to another point(x:300, y:100)
-c.lineTo(300,100);
-// actually draw the stroke of the path
-c.stroke();
-// close the path
+// Define an arc of a circle with center at point(x:200, y:200) with radius of 50px
+// arc starts from 0 degree to full 2*pie angle, thus creating a full circle
+c.arc(200, 200, 50, 0, Math.PI * 2, false);
+// assign color to fill with a hexcode. Default if black fill
+c.fillStyle = "#6af593";
+// actually fill the path
+c.fill();
 c.closePath();
 
