669dd362fe4b8c8d614a638547fc91fa50a55d98
Abhishek Malik <abhimskywalker@gmail.com>
2018-05-05 23:39:16 +0530

    Cleaner code and now Circle has x velocity
    Moved repeat draw code in `update` function of `Circle` class.

--- src/app.js
+++ src/app.js
@@ -11,11 +11,17 @@ canvas.height = window.innerHeight
 
 // Circle class to creat more circle objects easily later
 class Circle {
-    constructor(x, y, radius, color) {
+    constructor(x, y, radius, color, dx) {
         this.x = x;
         this.y = y;
         this.radius = radius;
         this.color = color;
+        // x velocity
+        this.dx = dx;
+    }
+    update() {
+        this.x = this.x + this.dx;
+        this.draw();
     }
     draw() {
         c.beginPath();
@@ -31,7 +37,7 @@ let circle;
 
 // Function to initiate and setup our starting canvas. This is called one time only as below at end of file
 function init() {
-    circle = new Circle(200, 200, 50, '#6af593');
+    circle = new Circle(200, 200, 50, '#6af593', 1);
     circle.draw();
 }
 
@@ -42,10 +48,8 @@ function animate() {
     // Next we clear the canvas to start fresh
     c.clearRect(0, 0, canvas.width, canvas.height)
 
-    // We move the centre x by 1
-    circle.x += 1;
-    // Next we draw this moved circle on canvas
-    circle.draw();
+    // Next we update this circle on canvas (repeat draw code abstracted in update function of Circle class)
+    circle.update();
 }
 
 // Initiate setup of canvas
