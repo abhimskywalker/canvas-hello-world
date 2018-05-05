5e2a600e21af759e88f2b8def96d5e9f984d71dd
Abhishek Malik <abhimskywalker@gmail.com>
2018-05-05 23:09:17 +0530

    Add Canvas to them and draw a 200px horizontal line
    Simple step to test canvas is working fine and get started.

--- src/app.js
+++ src/app.js
@@ -1,2 +1,22 @@
 import './assets/scss/app.scss';
 console.log('Its working just fine');
+
+// Initial Setup
+const canvas = document.querySelector('canvas')
+const c = canvas.getContext('2d')
+
+// Make sure canvas occupies all of the visible space in browser window
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight
+
+// begin drawing a path
+c.beginPath();
+// move to a starting point without drawing anything
+c.moveTo(100,100);
+// define a path line from starting point (x:100, y:100) to another point(x:300, y:100)
+c.lineTo(300,100);
+// actually draw the stroke of the path
+c.stroke();
+// close the path
+c.closePath();
+
